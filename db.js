8760b73fbfbbf101f0687b53de60db26bb997935
Shane Boehner <csgomakesmemad@gmail.com>
2015-07-12 01:44:48 -0600

    Added basic clientlist sync and authorization stuff.

---
+++ db.js
@@ -0,0 +1,84 @@
+var async = require('async');
+var mysql = require('mysql2');
+var connection;
+
+var ctx = {
+	dbhost: "",
+	dbuser: "",
+	dbpass: "",
+	dbname: ""
+};
+
+function connect(dbhost, dbuser, dbpass, dbname) {
+	ctx.dbhost = dbhost;
+	ctx.dbuser = dbuser;
+	ctx.dbpass = dbpass;
+	ctx.dbname = dbname;
+
+
+	console.log("Connecting to MySQL server...");
+	connection = mysql.createConnection({
+		host: dbhost,
+		port: 3306,
+		user: dbuser,
+		password: dbpass,
+		database: dbname
+	})
+
+	connection.on('error', function(err, test) {
+		console.log("Error connecting to MySQL server.");
+		connect(dbhost, dbuser, dbpass, dbname);
+	})
+}
+
+exports.init = function(dbhost, dbuser, dbpass, dbname) {
+	connect(dbhost, dbuser, dbpass, dbname);
+}
+
+exports.query = function(query, params, callback) {
+	console.log('PERFORMING QUERY ' + query);
+	console.log('	PARAMS: ' + params);
+
+	connection.execute(query, params, function(err, rows, fields) {
+		callback(err, rows);
+	})
+}
+
+// transaction system; we spawn a new connection for each transaction
+exports.transaction = function(queries, cb) {
+	var newconnection = mysql.createConnection({
+		host: ctx.dbhost,
+		port: 3306,
+		user: ctx.dbuser,
+		password: ctx.dbpass,
+		database: ctx.dbname
+	})
+
+	newconnection.beginTransaction(function(err) {
+		if (err) {
+			cb(null);
+			return;
+		}
+		async.eachSeries(queries, function(query, next) {
+			newconnection.query(query[0], query[1], function(err) {
+				if (err) {
+					next(err)
+				} else {
+					next(null)
+				}
+			})
+		}, function(err) {
+			if (err) {
+				cb(err);
+				return;
+			}
+			newconnection.commit(function(err) {
+				if (err) {
+					cb(err)
+				} else {
+					cb(null)
+				}
+			})
+		})
+	})
+}
--- index.js
+++ index.js
@@ -1,5 +1,81 @@
 // quibs.org plugins for dobby
 
-exports.config = function() {
-	
+var db = require('./db');
+var async = require('async');
+
+exports.config = function(cfg) {
+    db.init(cfg.dbhost, cfg.dbuser, cfg.dbpass, cfg.dbname)
+}
+
+function auth_data(dobby, cb) {
+    dobby.client_from.get_uid(function(err, uid) {
+        if (!err) {
+            db.query("SELECT * FROM users WHERE `teamspeak_uid`=?", [uid], function(err, results) {
+                if (err) {
+                    cb(err)
+                } else {
+                    if (results.length == 1) {
+                        cb(null, results[0])
+                    } else {
+                        cb(null, false)
+                    }
+                }
+            })
+        } else {
+            cb(err)
+        }
+    })
+}
+
+exports.init = function(dobby) {
+    async.forever(function(next) {
+        dobby.client_list(function(err, list) {
+            if (!err) {
+                async.map(list, function(client, cb) {
+                    client.update(function() {
+                        client.disable_updates();
+
+                        async.series({
+                            uid: function(cb) {client.get_uid(cb)},
+                            ip: function(cb) {client.get_ip(cb)},
+                            nickname: function(cb) {client.get_name(cb)},
+                            cid: function(cb) {client.get_cid(cb)},
+                            clid: function(cb) {client.get_clid(cb)}
+                        }, function(err, results) {
+                            cb(null, ["INSERT INTO teamspeak_users (ip, nickname, uid, cid, clid) VALUES (?, ?, ?, ?, ?)", [results.ip, results.nickname, results.uid, results.cid, results.clid]])
+                        })
+                    })
+                }, function(err, list) {
+                    if (!err) {
+                        list.unshift(["DELETE FROM teamspeak_users", []]);
+                        db.transaction(list, function(err) {
+                            setTimeout(next, 5000);
+                        })
+                    } else {
+                        setTimeout(next, 5000);
+                    }
+                })
+            } else {
+                setTimeout(next, 5000);
+            }
+        })
+    })
+}
+
+exports.onMessage = function(msg, dobby) {
+    var terms = msg.split(" ");
+    var command = terms.shift();
+    terms = terms.join(" ");
+
+    switch (command) {
+        case '.whoami':
+            auth_data(dobby, function(err, auth) {
+                if (auth) {
+                    dobby.respond("You are " + auth.username + " on quibs.org. :)")
+                } else {
+                    dobby.respond("I don't know. Have you signed up on quibs.org and/or linked your account?")
+                }
+            })
+        break;
+    }
 }
